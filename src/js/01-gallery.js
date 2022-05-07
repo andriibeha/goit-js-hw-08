13d9f0ecb28710c2e5ee80ae79669dd7b548be53
andriibeha <beha.andrii@gmail.com>
2022-05-07 12:18:36 +0300

    JS-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,29 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// Add imports simplelightbox
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryList = document.querySelector('.gallery');
+const markupGallery = createMarkupGallery(galleryItems);
+
+galleryList.insertAdjacentHTML('beforeend', markupGallery);
+
+// Создание динамической разметки
+function createMarkupGallery(array) {
+  return array
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" style="display: block" />
+        </a>`;
+    })
+    .join('');
+}
+
+// Создание и настройка библиотеки simplelightbox
+new SimpleLightbox('.gallery a', {
+  showCounter: false,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
